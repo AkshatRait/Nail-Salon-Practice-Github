a5c134b0de46384d2936300dcc3e099c6f4821a3
unknown <akshatrait@gmail.com>
2023-11-15 18:45:05 -0800

    testing with main bracnh

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>Welcome to practice App</h1>
+        <p>making changes in the main branch</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
